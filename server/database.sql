79c5013d1fb7b56e96eb6542bbf21b1623f689ce
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-04-01 17:16:13 +0530

    update: insert/post method complete

--- server/database.sql
+++ server/database.sql
@@ -13,9 +13,25 @@ CREATE TABLE users (
   hashed_password VARCHAR(255)
 );
 
+-- create/insert syntax
 INSERT INTO checklists(
   id, user_email, title, progress, date
 ) VALUES(
-  '1', 'abhijay@test.com', 'macbook local checklist', 3, '2024-03-28'
+  '0', 'abhijay@test.com', 'modified first checklist', 12, '2024-04-01'
 );
 
+
+INSERT INTO checklists (id, user_email, title, progress, date) VALUES ('4', 'abhijay@test.com', 'another checklist from express', 50, '2024-03-28');
+
+
+
+-- Read syntax
+SELECT * FROM checklists;
+
+-- update/edit syntax
+UPDATE checklists SET user_email = '', title = '', progress = 100 WHERE id = '';
+
+UPDATE checklists SET user_email = 'abhijay@test.com', title = 'modified checklist', progress = 80 WHERE id = '0';
+
+-- delete syntax
+DELETE FROM checklists WHERE id = '0';
--- server/server.js
+++ server/server.js
@@ -3,21 +3,46 @@ const express = require('express');
 const cors = require('cors')
 const app = express()
 const pool = require('./db')
+const { v4:uuidv4 } = require('uuid')
 
 app.use(cors())
+app.use(express.json()) // now express can use and parse json, yayy!
+
+app.get('/checklists/', async (req, res) => {
+  try {
+    const response = await pool.query('SELECT * FROM checklists;')
+    res.send(response.rows)
+  } catch (error) {
+    console.log('error:', error)
+  }
+})
 
 // get all checklists...
 app.get('/checklists/:userEmail', async (req, res) => {
   const { userEmail } = req.params;
   try {
     const checklists = await pool.query('SELECT * FROM checklists WHERE user_email = $1', [userEmail])
-    console.log(checklists)
     res.json(checklists.rows)
   } catch (error) {
     console.log(console.error(error));
   }
 })
 
+// create a new checklist
+app.post('/checklists', async (req, res) => {
+  const id = uuidv4()
+  const { userEmail, title, progress, date } = req.body; 
+  try {
+    await pool.query('INSERT INTO checklists (id, user_email, title, progress, date) VALUES ($1, $2, $3, $4, $5);', 
+    [id, userEmail, title, progress, date])
+
+    res.send({ msg: 'success' })
+  } 
+
+  catch (error) {
+    console.error(error)
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`server running on PORT ${PORT}`)
