09e7480cc59c94caea8fb7d4519bcbb37693bd54
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-04-04 10:11:07 +0530

    update: db env

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool
 // require('dotenv').config()
 
 const pool = new Pool ({
-  user: process.env.USER,
-  password: process.env.PASSWORD,
   host: process.env.HOST,
   port: process.env.DBPORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
   database: 'checklistapp'
 });
 
